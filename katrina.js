51d6f09e3cfadda23636204fae07481dc9200043
katriiinaak <102228708+katriiinaak@users.noreply.github.com>
2023-05-22 13:44:07 +0300

    ok

--- katrina.js
+++ katrina.js
@@ -1,6 +1,6 @@
 document.getElementById("myButton").addEventListener("click",function(){alert("Button clicked!");});
 
 function displayDateTime(){
-    const now = new Date();
-    document.getElementById("demo").innerHTML = now;
+    x = new Date();
+    document.getElementById("demo").innerHTML = x;
 }
